fe487b6f905089e815f7f947328ad5fbfd30ebaf
BrooksPoltl <bpoltl1@gmail.com>
2019-05-18 17:32:57 -0500

    can get stock data and format it into an array to perform calculations on it

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
 import config
+import requests
 key = config.ALPHA_API_KEY
-import csv
-from alpha_vantage.timeseries import TimeSeries
-import matplotlib.pyplot as plt
-
-
-ts = TimeSeries(key=key, output_format='csv')
-data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
-print(data)
-#whoops
-for row in data:
-    print(row)
+output = 40
+index = 0
+stock_data= requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=MSFT&apikey=%s',key)
+stock_data = data.json()
+adjusted_stock_prices = []
+stock_data = stock_data["Monthly Adjusted Time Series"]
+for index,row in enumerate(stock_data):
+    if(index > output):
+        break
+    adjusted_stock_prices.append(stock_data[row]["5. adjusted close"])
+print(adjusted_stock_prices)
+print(len(adjusted_stock_prices))
