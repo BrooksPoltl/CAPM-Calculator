942a9d5550567511a5a5d46ebbc3da3d930a8d13
BrooksPoltl <bpoltl1@gmail.com>
2019-05-18 16:56:03 -0500

    can get data from the frontend

--- index.py
+++ index.py
@@ -1,3 +1,12 @@
 import config
 key = config.ALPHA_API_KEY
-print(key)
+import csv
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+
+
+ts = TimeSeries(key=key, output_format='csv')
+data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+print(data)
+for row in data:
+    print(row)
